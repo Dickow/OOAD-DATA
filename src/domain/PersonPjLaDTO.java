a13f404a9390e0a2cb86f54104c8b4c103ae2cb3
BlibberBlobber <Mortenvorborg@hotmail.com>
2014-11-06 11:19:53 +0100

    PersonPjLa done

--- src/domain/PersonPjLaDTO.java
+++ src/domain/PersonPjLaDTO.java
@@ -1,29 +1,60 @@
 package domain;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import data.Connector;
+import data.DALException;
+
 public class PersonPjLaDTO {
+
+	private int id;
+	private String previousJobs, language;
+
+	public PersonPjLaDTO(String previousJobs, String language, int id) {
+
+	}
+
+	public String getPreviousJobs() {
+		return previousJobs;
+	}
+
+	public void setPreviousJobs(String previousJobs) {
+		this.previousJobs = previousJobs;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
 	
-	 private List<String> previousJobs, language;
-	 
-	 public PersonPjLaDTO(List<String> previousJobs, List<String> language){
-		 
-	 }
-	 
-	 public List<String> getPreviousJobs() {
-			return previousJobs;
+	}
+	/*
+	 * public List<PersonPjLaDTO> findPersonPjLa(int id) throws DALException, SQLException {
+		try {
+			Connector.connect();
+		} catch (Exception e1) {
+			throw new DALException(
+					"Der kunne ikke oprettes forbindelse til databasen");
 		}
 
-		public void setPreviousJobs(List<String> previousJobs) {
-			this.previousJobs = previousJobs;
-		}
+		List<PersonPjLaDTO> list = new ArrayList<PersonPjLaDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM PersonPjLa WHERE Id = "
+				+ id);
+		Connector.closeConnection();
 
-		public List<String> getLanguage() {
-			return language;
+		while (rs.next()) {
+			list.add(new PersonPjLaDTO(rs.getString("previousJobs"), rs
+					.getString("language"), rs.getInt("id")));
 		}
-
-		public void setLanguage(List<String> language) {
-			this.language = language;
-		}
-	 
+		return list;
+	}
+	 */
+	
 }
+
+
