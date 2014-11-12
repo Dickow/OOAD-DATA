31ac513b8629b781783bfbc1dd976b049dde6cab
Dickow <Dickow@82.211.207.27>
2014-11-12 17:53:00 +0100

    ny styling

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -25,58 +25,35 @@ public class Login extends JPanel {
 	 * Create the panel.
 	 */
 	public Login() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
-		
-		JLabel brugerId = new JLabel("Bruger id");
-		GridBagConstraints gbc_label = new GridBagConstraints();
-		gbc_label.insets = new Insets(0, 0, 5, 5);
-		gbc_label.gridx = 6;
-		gbc_label.gridy = 3;
-		add(brugerId, gbc_label);
-		
-		brugerIdTextField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 6;
-		gbc_textField.gridy = 4;
-		add(brugerIdTextField, gbc_textField);
-		brugerIdTextField.setColumns(10);
-		
-		JLabel password = new JLabel("Password");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 6;
-		gbc_lblNewLabel.gridy = 5;
-		add(password, gbc_lblNewLabel);
+		setLayout(null);
 		
 		passwordField = new JPasswordField();
-		GridBagConstraints gbc_passwordField = new GridBagConstraints();
-		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_passwordField.anchor = GridBagConstraints.NORTH;
-		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
-		gbc_passwordField.gridx = 6;
-		gbc_passwordField.gridy = 6;
-		add(passwordField, gbc_passwordField);
+		passwordField.setBounds(163, 125, 116, 22);
+		add(passwordField);
+		this.add(passwordField);
+		
+		brugerIdTextField = new JTextField();
+		brugerIdTextField.setBounds(163, 78, 116, 22);
+		add(brugerIdTextField);
+		brugerIdTextField.setColumns(10);
+		this.add(brugerIdTextField);
 		
 		JButton loginButton = new JButton("Log ind");
+		loginButton.setBounds(183, 160, 73, 25);
 		loginButton.setAction(action);
 		loginButton.setActionCommand("login");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 6;
-		gbc_btnNewButton.gridy = 7;
-		add(loginButton, gbc_btnNewButton);
+		add(loginButton);
 		this.add(loginButton);
-		this.add(password);
+		
+		JLabel brugerId = new JLabel("Bruger id");
+		brugerId.setBounds(190, 59, 52, 16);
+		add(brugerId);
 		this.add(brugerId);
-		this.add(brugerIdTextField);
-		this.add(passwordField);
+		
+		JLabel password = new JLabel("Password");
+		password.setBounds(190, 109, 55, 16);
+		add(password);
+		this.add(password);
 		
 
 	}
