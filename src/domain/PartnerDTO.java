3f90ecac7cba9a2f97e351ee4a6f6d3b0bb23d44
Nikolaj <Nikobirck@hotmailcom>
2014-11-13 10:02:36 +0100

    ændret findResearcherOnCase

---
+++ src/domain/PartnerDTO.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class PartnerDTO extends EmployeeDTO {
+
+	public PartnerDTO(int employeeId, String name, String password, JOB job) {
+		super(employeeId, name, password, job);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
