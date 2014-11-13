7a19ed793bbfa92cc1e03a515b8dba50a0b315af
Dickow <Dickow@10.16.160.38>
2014-11-13 09:01:11 +0100

    constructors til partner og researcher

--- src/domain/ResearcherDTO.java
+++ src/domain/ResearcherDTO.java
@@ -2,4 +2,9 @@ package domain;
 
 public class ResearcherDTO extends EmployeeDTO{
 
+	public ResearcherDTO(int employeeId, String name, String password, JOB job) {
+		super(employeeId, name, password, job);
+		// TODO Auto-generated constructor stub
+	}
+
 }
