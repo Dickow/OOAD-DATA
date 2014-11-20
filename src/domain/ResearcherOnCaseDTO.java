088023e4c6d81e330b5fee6d4af1b74f55c2c986
Nikolaj <Nikobirck@hotmailcom>
2014-11-20 10:04:55 +0100

    ;)

--- src/domain/ResearcherOnCaseDTO.java
+++ src/domain/ResearcherOnCaseDTO.java
@@ -2,8 +2,19 @@ package domain;
 
 public class ResearcherOnCaseDTO {
 
+	int researcherId;
+	String caseName;
+	
 	public ResearcherOnCaseDTO(int researcherId, String caseName) {
 		// TODO Auto-generated constructor stub
 	}
 
+	public int getResearcherId(){
+		return researcherId;
+	}
+	
+	
+	public String getcaseName(){
+		return caseName;
+	}
 }
