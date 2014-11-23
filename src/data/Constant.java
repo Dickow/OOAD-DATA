53428f212b24fc197556e15db868306abba96859
Dickow <Dickow@82.211.207.27>
2014-11-23 15:01:48 +0100

    øøhm, der var vist en driver der manglede, nu kan vi connecte til databasen

--- src/data/Constant.java
+++ src/data/Constant.java
@@ -6,13 +6,10 @@ package data;
  * @author Group 5
  * 
  */
-public abstract class Constant {
+public abstract class Constant { 
 	public static final 
 	String server = "localhost", database = "odgersberndtson",username = "root", password = "1234";
-//	 public static final String server = "localhost", database = "odgersberndtson",
-//	 username = "root", password = "1234";
 
 	// port number to establish connection
 	public static final int port = 3306;
 }
-
